95f2753753698da4aa63afe09ffe670eb67e9e7c
Dmitrii <dimasioeremin@gmail.com>
2024-01-24 09:10:18 +0200

    added .env and updated api config

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-	apiUrl: 'https://api.react-learning.ru/v2/sb-4',
+	apiUrl: `${process.env.API_URL}`,
 	apiToken:
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFhNDA0MWVhNjJiZjA0MGJkYmRiMTEiLCJncm91cCI6InNiLTQiLCJpYXQiOjE3MDU2NTY1MTEsImV4cCI6MTczNzE5MjUxMX0.iCDFRzhfCsmKibmpnAKDacRSH6dT7IZUfOFfYLzIegU',
 };
