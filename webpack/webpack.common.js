cac05891925fa95dc7b5ec0553f18fca6dcb7be0
Dmitrii <dimasioeremin@gmail.com>
2024-01-24 12:33:41 +0200

    update publicPath

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
 		filename: production
 			? 'static/scripts/[name].[contenthash].js'
 			: 'static/scripts/[name].js', // имя нашего бандла
-		publicPath: process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '/',
+		publicPath: 'https://zebrooo.github.io/psychic-waffle.github.io',
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
 	module: {
