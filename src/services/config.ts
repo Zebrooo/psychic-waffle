3c08ad50bbd346aa48a4c3990c87cbf53a6e0a38
Dmitrii <dimasioeremin@gmail.com>
2024-01-24 12:08:29 +0200

    update api config

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-	apiUrl: `${process.env.API_URL}`,
+	apiUrl: process.env.API_URL,
 	apiToken:
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFhNDA0MWVhNjJiZjA0MGJkYmRiMTEiLCJncm91cCI6InNiLTQiLCJpYXQiOjE3MDU2NTY1MTEsImV4cCI6MTczNzE5MjUxMX0.iCDFRzhfCsmKibmpnAKDacRSH6dT7IZUfOFfYLzIegU',
 };
